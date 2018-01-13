3ee8fdb324e449cf3e304de1f24e2eb0af6bb53d
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-01-13 18:09:34 -0200

    - *ngFor e afins!

--- Estudos_Angular/ClientApp/app/components/app/app.component.ts
+++ Estudos_Angular/ClientApp/app/components/app/app.component.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     courses = [1, 2];
+    newCourses = [
+        { id: 1, name: 'C#' },
+        { id: 2, name: 'JAVA' },
+        { id: 3, name: 'REACT' },
+        { id: 4, name: 'ANGULAR' },
+        { id: 5, name: 'IONIC' },
+        { id: 6, name: 'SHAREPOINT' },
+        { id: 7, name: '.NETCORE' }
+    ];
+
+
+    viewMode = 'list';
 
     post = {
         title: "Title",
@@ -27,9 +39,20 @@ export class AppComponent {
     }
     mostraLista() {
         let result;
-
-        result = (this.courses.length > 0) ? true : false
-
+        result = (this.courses.length > 0) ? true : false 
         return result;
     }
+    onAdd() {
+        this.newCourses.push({ id: 8, name: 'ASP' });
+    }
+    removeNewCourse(curso: any) {
+
+        let index = this.newCourses.indexOf(curso);
+        console.log('Index', index);
+
+        this.newCourses.splice(index, 1); //remove o index somente, se nao passar o 1, remove do index pra baixo!!
+
+        console.log('removido', this.newCourses);
+    }
+
 }
