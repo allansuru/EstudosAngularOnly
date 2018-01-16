65fd93ce9b84280e063e22aed7d950b5e54f742a
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-01-16 18:58:35 -0200

    - Novas técnicas para Form!

--- Estudos_Angular/ClientApp/app/components/contact/contact.component.ts
+++ Estudos_Angular/ClientApp/app/components/contact/contact.component.ts
@@ -8,23 +8,33 @@
 
 export class ContactComponent implements OnInit {
 
-    contact =  {
-        firstName: ''
+    contact = {
+        firstName: '',
+        comment: '',
+        isSubscribed: '',
+        contactMethod: ''
     }
+
+
+    contactMethodOptions =  [
+        { id: 1, name: 'Email' },
+        { id: 2, name: 'Phone' }
+    ]
+
 
     ngOnInit() {
-       // console.log(this.contact);
+        // console.log(this.contact);
     }
 
-    log(ngModel: any, form:any) {
+    log(ngModel: any, form: any) {
         console.log('LOG', ngModel);
-        //console.log('FORM', form);
-        
+        console.log('FORM', form);
+
     }
     submit(f: any) {
         console.log('submit', f);
         console.log('Valores do form', JSON.stringify(f.value));
-        
+
     }
-    
+
 }
