554043a1d5d60ec83505066243cae4f8cad38b4f
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-01-15 23:44:15 -0200

    - ngForm

--- Estudos_Angular/ClientApp/app/components/contact/contact.component.ts
+++ Estudos_Angular/ClientApp/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component } from '@angular/core'
+﻿import { Component, OnInit } from '@angular/core'
 
 @Component({
     selector: 'contact-form',
@@ -6,10 +6,25 @@
     styleUrls: ['./contact.component.css']
 })
 
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
-    log(x: any) {
-        console.log('LOG', x)
+    contact =  {
+        firstName: ''
+    }
+
+    ngOnInit() {
+       // console.log(this.contact);
+    }
+
+    log(ngModel: any, form:any) {
+        console.log('LOG', ngModel);
+        //console.log('FORM', form);
+        
+    }
+    submit(f: any) {
+        console.log('submit', f);
+        console.log('Valores do form', JSON.stringify(f.value));
+        
     }
     
 }
