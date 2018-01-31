64c543d36969d30433575dba898b40e7a97a6886
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-01-31 21:21:23 -0200

    - JWT estudos

--- Estudos_Angular/ClientApp/app/services/auth.service.ts
+++ Estudos_Angular/ClientApp/app/services/auth.service.ts
@@ -1,5 +1,9 @@
 ﻿import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { Observable } from "rxjs/Observable";
+import { AuthHttp, AuthConfig, JwtHelper, tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -7,15 +11,44 @@ export class AuthService {
     }
 
     login(credentials: any) {
-        return this.http.post('/api/authenticate',
-            JSON.stringify(credentials));
+        return this.http.post('/api/authenticate', JSON.stringify(credentials))
+            .map(response => {
+                let result = response.json();
+                if (result && result.token) {
+                    localStorage.setItem('token', result.token);
+                    console.log('Result: ', result);
+                    return true;
+                }
+                return false;
+            
+            })
+            .catch(this.handleError);
     }
 
     logout() {
+        localStorage.removeItem('token');
     }
 
     isLoggedIn() {
-        return false;
+        return tokenNotExpired();
+            
+     /*   let jwtHelper = new JwtHelper();
+        let token = localStorage.getItem('token');
+
+        if (!token)
+            return false;
+
+        let expirationDate = jwtHelper.getTokenExpirationDate(token as string);
+        let isExpired = jwtHelper.isTokenExpired(token as string);
+
+        console.log('Expiration: ', expirationDate);
+        console.log('isExpired: ', isExpired)
+
+        return !isExpired; */
+    }
+    private handleError(error: Response) {
+        console.log('Error: ', error);
+        return Observable.throw(error.json());
     }
 }
 
